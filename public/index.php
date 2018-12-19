0cb16f7e728b0a77aac904aaeb2c107a038b7537
juve534 <maxim_4141.rock@zoho.com>
2018-12-20 02:17:24 +0900

    [init] Slimの初期化
    Composerでslimをインストールし、hello worldを実施。

---
+++ public/index.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types=1);
+
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$app = new \Slim\App;
+$app->get('/hello/{name}', function (Request $request, Response $response) {
+    $name = $request->getAttribute('name');
+    $response->getBody()->write("Hello, $name");
+
+    return $response;
+});
+$app->run();
